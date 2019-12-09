0f66c3b67eca2aa0f240d2bcc5a0122244c4a0b1
Yohan Dunon <55760027+DYW972@users.noreply.github.com>
2019-12-09 20:40:55 +0100

    added buttons and images view outlets and button actions

--- InstaGrid/InstaGrid/Controller/ViewController.swift
+++ InstaGrid/InstaGrid/Controller/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK:
+    
+    @IBOutlet weak var addPictureBtn1: UIButton!
+    @IBOutlet weak var addPictureBtn2: UIButton!
+    @IBOutlet weak var addPictureBtn3: UIButton!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var layout1: UIImageView!
+    @IBOutlet weak var layout2: UIImageView!
+    @IBOutlet weak var layout3: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: Actions
 
-
+    @IBAction func addPictureBtn1WasPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func addPictureBtn2WasPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func addPictureBtn3WasPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
